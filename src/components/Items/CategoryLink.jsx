1b3b50cae64895e4b631ad532e9e30953334b41f
Edward Guo <edward.h.guo@gmail.com>
2016-11-12 13:05:31 -0800

    Item category icons up

--- src/components/Items/CategoryLink.jsx
+++ src/components/Items/CategoryLink.jsx
@@ -2,4 +2,11 @@ import React from 'react';
 import { Link } from 'react-router';
 
 const className = 'col-xs-2';
-export default ({category}) => <Link className={className} to={`/items/${category.toLowerCase()}`}>{category}</Link>
+export default ({category}) => {
+  console.log(category);
+  const imgURL = `../../assets/ItemIcons/${category}.png`;
+  const style = {
+    backgroundImage: `url(${imgURL})`
+  }
+  return <Link className={className} to={`/items/${category.toLowerCase()}`} style={style}>{category}</Link>
+}
